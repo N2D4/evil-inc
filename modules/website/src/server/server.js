9d3516f3eb0accd1fc2b056141af910d244e989c
Stan Wohlwend <n2d4xc@gmail.com>
2021-03-26 19:17:58 +0100

    Website module

--- modules/website/src/server/server.js
+++ modules/website/src/server/server.js
@@ -1,25 +1,29 @@
+require('core-js/stable');
+require('regenerator-runtime/runtime');
 const http = require('http');
 const util = require('util');
 const fs = require('fs');
-const child_process = require('child_process');
-
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('db/website.db', sqlite3.OPEN_READONLY);
 
 async function respondWithFile(res, filePath) {
     const file = await util.promisify(fs.readFile)(filePath);
     const fileType = filePath.endsWith('.html') ? 'text/html'
                     : filePath.endsWith('.css') ? 'text/css'
                     : filePath.endsWith('.txt') ? 'text/plain'
+                    : filePath.endsWith('.js') ? 'text/javascript'
                     : 'application/octet-stream';
     res.writeHead(200, {'Content-Type': fileType});
     res.end(file);
 }
 
-async function checkToken(token) {
-    return checkCredentials(...token.split(':'));
+async function checkToken(token, needsAdmin) {
+    return (!needsAdmin || token.startsWith('vanessa:')) && await checkCredentials(...token.split(':'));
 }
 
 async function checkCredentials(username, hashedPassword) {
-    return true;
+    const row = await util.promisify(db.get.bind(db))("SELECT hashedPassword FROM users WHERE name = '" + username + "'");
+    return row && row.hashedPassword === hashedPassword;
 }
 
 async function respond(req, res) {
@@ -36,22 +40,67 @@ async function respond(req, res) {
         }
         case '/admin': {
             const cookie = req.headers.cookie;
-            if (cookie && checkToken(cookie.split('=')[1])) {
+            if (cookie && await checkToken(cookie.split('=')[1], true)) {
                 return await respondWithFile(res, 'src/client/admin.html');
+            } else {
+                return await respondWithFile(res, 'src/client/access-denied.html');
+            }
+        }
+        case '/publish': {
+            const cookie = req.headers.cookie;
+            if (cookie && await checkToken(cookie.split('=')[1], true)) {
+                return await respondWithFile(res, 'src/client/publish.html');
+            } else {
+                return await respondWithFile(res, 'src/client/access-denied.html');
+            }
+        }
+        case '/stop-server': {
+            const cookie = req.headers.cookie;
+            if (cookie && await checkToken(cookie.split('=')[1], true)) {
+                console.log('Server stop requested');
+                process.exit(0);
+            } else {
+                return await respondWithFile(res, 'src/client/access-denied.html');
+            }
+        }
+        case '/users': {
+            const cookie = req.headers.cookie;
+            if (cookie && await checkToken(cookie.split('=')[1], true)) {
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify(await util.promisify(db.all.bind(db))("SELECT name FROM users")));
+                return;
+            } else {
+                return await respondWithFile(res, 'src/client/access-denied.html');
+            }
+        }
+        case '/source': {
+            const cookie = req.headers.cookie;
+            if (cookie && await checkToken(cookie.split('=')[1], true)) {
+                return await respondWithFile(res, __filename);
             } else {
                 return await respondWithFile(res, 'src/client/access-denied.html');
             }
         }
         case '/register': {
             if (req.method === 'POST') {
-                let body = '';
-                req.on('data', chunk => {
-                    body += chunk.toString();
-                });
-                req.on('end', () => {
-                    const [username, hash] = JSON.parse(body);
-                    res.writeHead(200, {'Content-Type': 'application/json'});
-                    res.end('{"msg": "You have been registered!"}');
+                return new Promise((resolve, reject) => {
+                    let body = '';
+                    req.on('data', chunk => {
+                        body += chunk.toString();
+                    });
+                    req.on('end', () => {
+                        const [username, hash] = JSON.parse(body);
+                        const query = "INSERT INTO users(name, hashedPassword) VALUES('" + username + "', '" + hash + "')";
+                        db.run(query, (err) => {
+                            if (err) {
+                                reject(err);
+                            } else {
+                                res.writeHead(200, {'Content-Type': 'application/json'});
+                                res.end('{"msg": "You have been registered!", "redirect": "/login"}');
+                                resolve();
+                            }
+                        });
+                    });
                 });
             } else {
                 return await respondWithFile(res, 'src/client/register.html');
@@ -59,20 +108,23 @@ async function respond(req, res) {
         }
         case '/login': {
             if (req.method === 'POST') {
-                let body = '';
-                req.on('data', chunk => {
-                    body += chunk.toString();
-                });
-                req.on('end', () => {
-                    const [username, hash] = JSON.parse(body);
-                    if (checkCredentials(username, hash)) {
-                        const token = `${username}:${hash}`;
-                        res.writeHead(200, {'Content-Type': 'application/json'});
-                        res.end(`{"msg": "Log-in successful!", "token": "${token}"}`);
-                    } else {
-                        res.writeHead(200, {'Content-Type': 'application/json'});
-                        res.end(`{"msg": "Error! Invalid credentials?"}`);
-                    }
+                return new Promise((resolve, reject) => {
+                    let body = '';
+                    req.on('data', chunk => {
+                        body += chunk.toString();
+                    });
+                    req.on('end', async () => {
+                        const [username, hash] = JSON.parse(body);
+                        if (await checkCredentials(username, hash)) {
+                            const token = `${username}:${hash}`;
+                            res.writeHead(200, {'Content-Type': 'application/json'});
+                            res.end(`{"msg": "Log-in successful!", "token": "${token}", "redirect": "/"}`);
+                        } else {
+                            res.writeHead(200, {'Content-Type': 'application/json'});
+                            res.end(`{"msg": "Error! Invalid credentials?"}`);
+                        }
+                        resolve();
+                    });
                 });
             } else {
                 return await respondWithFile(res, 'src/client/login.html');
@@ -91,8 +143,8 @@ async function respond(req, res) {
                 if (subUrl.startsWith('/')) {
                     subUrl = subUrl.substring(1);
                 }
-                if (subUrl.startsWith('..')) {
-                    throw new Error(`Security alert`);
+                if (subUrl.startsWith('.')) {
+                    throw new Error(`Path traversal attack alert`);
                 }
 
                 const filePath = './assets/' + subUrl;
@@ -128,9 +180,9 @@ const server = http.createServer(async (req, res) => {
     try {
         await respond(req, res);
     } catch (e) {
-        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.writeHead(500, {'Content-Type': 'application/json'});
         console.log(e);
-        res.end(`Error! ${e.name}: ${e.message}`);
+        res.end(JSON.stringify({error: `${e.name}: ${e.message}`}));
     }
 });
 
